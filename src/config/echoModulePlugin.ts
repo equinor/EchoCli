2e4d0854ca907d9267fcf801d3d701c809b0e1a5
Christopher Berge Hove <chris@nogglings.com>
2021-04-28 21:28:57 +0200

    structure

--- src/config/echoModulePlugin.ts
+++ src/config/echoModulePlugin.ts
@@ -1,25 +1,31 @@
-import fs from "fs";
-import { InputOptions, OutputOptions } from "rollup";
+import fs from 'fs';
+import { InputOptions, OutputOptions } from 'rollup';
 
-export default function echoModuleCreator() {
-  let fileName = "";
-  return {
-    name: "echo-moduleCreator",
-    async buildStart(options: InputOptions) {
-      fileName = options.input!.toString();
-    },
-    async writeBundle(options: OutputOptions, bundle) {
-      const config = {
-        match: /\(function\(g\,f\)\{/,
-        replace: `(function(g,f){function define(d, f){typeof document !== "undefined" && (document.currentScript.app = {});f(document.currentScript.app, ...d.filter((d) => d !== "exports").map(window.app));}define.amd = !0;`,
-      };
-      for (const [fileName, chunkOrAsset] of Object.entries(bundle)) {
-        if (bundle[fileName]) {
-          let data = fs.readFileSync(bundle[fileName], { encoding: "utf8" });
-          data = data.replace(config.match, config.replace);
-          fs.writeFileSync(options.file ?? fileName, data);
+interface EchoModuleCreator {
+    name: string;
+    buildStart(options: InputOptions): Promise<void>;
+    writeBundle(options: OutputOptions, bundle: any): Promise<void>;
+}
+
+export default function echoModuleCreator(): EchoModuleCreator {
+    let fileName = '';
+    return {
+        name: 'echo-moduleCreator',
+        async buildStart(options: InputOptions): Promise<void> {
+            fileName = options.input!.toString();
+        },
+        async writeBundle(options: OutputOptions, bundle): Promise<void> {
+            const config = {
+                match: /\(function\(g\,f\)\{/,
+                replace: `(function(g,f){function define(d, f){typeof document !== "undefined" && (document.currentScript.module = {});f(document.currentScript.module, ...d.filter((d) => d !== "exports").map(window.app));}define.amd = !0;`
+            };
+            for (const [fileName] of Object.entries(bundle)) {
+                if (bundle[fileName]) {
+                    let data = fs.readFileSync(bundle[fileName], { encoding: 'utf8' });
+                    data = data.replace(config.match, config.replace);
+                    fs.writeFileSync(options.file ?? fileName, data);
+                }
+            }
         }
-      }
-    },
-  };
+    };
 }
