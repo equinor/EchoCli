68af7b8f173a557142d6b157a1521d8a6b4e1078
Christopher Berge Hove <chris@nogglings.com>
2021-04-28 21:27:43 +0200

    template index with core api

--- templates/echoAppTemplate/src/index.ts
+++ templates/echoAppTemplate/src/index.ts
@@ -1,1 +1,6 @@
-export function setup() {}
+import { EchoModuleApi } from '@equinor/echo-core';
+import App from 'app';
+
+export function setup(api: EchoModuleApi): void {
+    api.registerApp(App);
+}
