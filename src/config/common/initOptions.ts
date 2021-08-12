6f5b8a28d25079fc676db6e1bd7fb1f6cb2f9556
Christopher Berge Hove <chris@nogglings.com>
2021-08-12 22:58:07 +0200

    echo config update to initial options for bundeling

--- src/config/common/initOptions.ts
+++ src/config/common/initOptions.ts
@@ -1,12 +1,11 @@
-import chalk from 'chalk';
-import * as fs from 'fs';
-import path, { join } from 'path';
+import path from 'path';
 import { OutputOptions, RollupOptions } from 'rollup';
-import { promisify } from 'util';
 import { Configuration } from 'webpack';
+import { REQUIRED_REF } from '../../const/common';
+import getFilePath from '../../utils/getFilePath';
+import { EchoModuleConfig, getEchoModuleConfig } from './echoModuleConfig';
+import getPackageJson from './getPackageJson';
 import { Https } from './https';
-
-const access = promisify(fs.access);
 
 export interface EchoRollupOptions extends EchoBundleOptions {
     inputOptions: RollupOptions;
@@ -29,14 +28,11 @@ export interface EchoBundleOptions {
     dependencies: Record<string, string>;
     devDependencies: Record<string, string>;
     requireRef: string;
-    name: string;
     https: Https;
-    shortName: string;
     envPath: string;
-}
-
-function getEnvFilePath(dir: string) {
-    return join(dir, './.env');
+    adminModule?: boolean;
+    adminModulePath: string;
+    echoModuleConfig: EchoModuleConfig;
 }
 
 export async function defineInitOptions(
@@ -44,34 +40,21 @@ export async function defineInitOptions(
     isDevelopment?: boolean
 ): Promise<Partial<EchoWebpackOptions | EchoRollupOptions>> {
     const currentDir = process.cwd();
-    const configPath = path.join(currentDir || '', '/package.json');
-
-    try {
-        await access(configPath, fs.constants.R_OK);
-    } catch (err) {
-        if (err instanceof Error) {
-            console.error('%s Cant access package.json', chalk.red.bold('ERROR'));
-            process.exit(1);
-        } else {
-            throw err;
-        }
-    }
-
-    const pkj = JSON.parse(fs.readFileSync(configPath).toString());
-
+    const echoModuleConfig: EchoModuleConfig = await getEchoModuleConfig(currentDir);
+    const pkj = await getPackageJson(currentDir);
     return {
         ...echoBundleOptions,
         isProduction: isDevelopment ? false : true,
         currentDir,
         source: pkj.source,
         main: pkj.main,
-        name: pkj.name,
-        shortName: pkj.manifest.shortName,
         peerDependencies: pkj.peerDependencies,
         dependencies: pkj.dependencies,
         devDependencies: pkj.devDependencies,
         wwwRoot: path.resolve(__dirname, '../../../', 'client'),
-        requireRef: 'echoDepLoader',
-        envPath: getEnvFilePath(currentDir)
+        adminModulePath: path.resolve(__dirname, '../../../', 'admin'),
+        requireRef: REQUIRED_REF,
+        envPath: await getFilePath(currentDir, './.env'),
+        echoModuleConfig
     };
 }
